c1e5083502f1a30d01f9bab0b445655986b2da96
Enrico Schumann <es@enricoschumann.net>
2013-05-08 17:48:59 +0200

    End-of-day commit (see ChangeLog).

--- R/returns.R
+++ R/returns.R
@@ -1,3 +1,18 @@
+drawdown <- function(v, relative = TRUE) {
+    cv  <- cummax(v)
+    rd  <- cv - v
+    if (relative)
+        rd  <- rd/cv
+    troughTime <- which.max(rd)
+    peakTime <- which.max(v[seq_len(troughTime)])
+    list(maximum = max(rd),
+         high = v[peakTime],
+         highPosition = peakTime,
+         low = v[troughTime],
+         lowPosition = troughTime)
+}
+
+## various functions to compute returns
 returns <- function(x, pad = NULL) {
     n <- NROW(x)
     do.pad <- !is.null(pad)
@@ -14,19 +29,6 @@ returns <- function(x, pad = NULL) {
     }
     rets
 }
-drawdown <- function(v, relative = TRUE) {
-    cv  <- cummax(v)
-    rd  <- cv - v
-    if (relative)
-        rd  <- rd/cv
-    troughTime <- which.max(rd)
-    peakTime <- which.max(v[seq_len(troughTime)])
-    list(maximum = max(rd),
-         high = v[peakTime],
-         highPosition = peakTime,
-         low = v[troughTime],
-         lowPosition = troughTime)
-}
 twReturns <- function(price, amount, pad = NULL) {
     if (missing(amount))
         returns(price, pad)
@@ -41,4 +43,28 @@ twReturns <- function(price, amount, pad = NULL) {
     if (do.pad) 
         rt <- c(pad, rt)
     rt
+}
+pReturns <- function(x, t = NULL, period) {
+
+    ## TODO: make internal function that checks x and t
+    if (is.null(t)) {
+        if (!inherits(x, "zoo")) {
+            stop(sQuote("t"), " not supplied, so ", sQuote("x"),
+                 " must inherit from ", sQuote("zoo"))
+        } else {
+            t <- index(x)
+            x <- coredata(x)
+        }
+    } else {
+        if (is.unsorted(t)) {
+            idx <- order(t)
+            t <- t[idx]
+            x <- x[idx]
+        }
+    }
+    if (grep("da(y|i)", period, ignore.case = TRUE)) {
+        by <- format(t, "%Y%m%d")
+        PMwR:::last(x, by)
+    }
+
 }
