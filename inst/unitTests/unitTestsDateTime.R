f730b08f0829b20fc2fd0998c7407378ceb4f0b1
Enrico Schumann <es@enricoschumann.net>
2013-05-23 17:43:04 +0200

    renamed several files

--- inst/unitTests/unitTestsDateTime.R
+++ inst/unitTests/unitTestsDateTime.R
@@ -1,5 +1,12 @@
 
-weekday <- 6
-x <- as.Date(c("1996-05-30","1996-05-20"))
-weekdays(x)
-lastWeekday(6, date, -1)
+## x <- as.Date(c("1996-05-30","1996-05-20"))
+## weekdays(x)
+## lastWeekday(6, x, -1)
+## lastWeekday(6, x, 0)
+
+
+## from <- ISOdatetime(2012,1,1,12,00,00)
+## to <- from + 36000
+## timegrid(from, to,
+##          interval = "15 sec",
+##          excludeWeekends = TRUE, holidays = NULL)
