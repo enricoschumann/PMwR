373272617c175200cbaa62cf697e72e764d7b1eb
Enrico Schumann <es@enricoschumann.net>
2013-07-24 09:36:54 +0200

    * DESCRIPTION (Version): Version: 0.1-34 * R/pm.R (pm): function added

---
+++ R/pm.R
@@ -0,0 +1,41 @@
+## -*- truncate-lines: t; -*-
+## Time-stamp: <2013-07-24 08:51:30 CEST (es)>
+
+pm <- function(x, xp = 2, threshold = 0, lower = TRUE,
+               normalise = FALSE, na.rm = FALSE) {
+
+    ## TODO: check normalise: only if xp is integer?
+    ## is.wholenumber <- function(x, tol = .Machine$double.eps^0.5)
+    ##     abs(x - round(x)) < tol
+
+    if (length(d <- dim(x))) {
+        n <- d[1L]
+        sum <- colSums
+    } else {
+        n <- length(x)
+    }
+    x <- x - threshold
+    if (lower)
+        x <- x - abs(x)
+    else
+        x <- x + abs(x)        
+    x <- abs(x)
+
+    ans <- if (xp == 1L)
+        sum(x, na.rm = na.rm)/2
+    else if (xp == 2L)
+        sum(x*x, na.rm = na.rm)/4
+    else if (xp == 3L)
+        sum(x*x*x, na.rm = na.rm)/8
+    else if (xp == 4L)
+        sum(x*x*x*x, na.rm = na.rm)/16
+    else 
+        sum(x^xp, na.rm = na.rm)/2^xp
+    ans <- ans/n
+
+    if (normalise)
+        ans^(1/xp)
+    else
+        ans
+
+}
