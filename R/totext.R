b820cd59c1532b65f09d391b2c36579ddfb44d3e
Enrico Schumann <es@enricoschumann.net>
2013-10-16 09:14:53 +0200

    small changes in toText

--- R/totext.R
+++ R/totext.R
@@ -1,9 +1,9 @@
 ## -*- truncate-lines: t; -*-
-## Time-stamp: <2013-09-28 13:03:02 CEST (es)>
+## Time-stamp: <2013-10-16 09:05:58 CEST (es)>
 
 toText <- function(x, ...)
     UseMethod("toText")
 
 toText.default <- function(x, ...) 
-    capture.output(write(x, ""))
+    capture.output(write(as.character(x), ""))
 
