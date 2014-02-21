75e7c2e7c1c7e52af41f464ed15fcbf7cdce06f5
Enrico Schumann <es@enricoschumann.net>
2014-02-21 21:05:44 +0100

    * R/NAVseries.R (NAVseries): new arguments "title" and "instrument" (summary.NAVseries): added function and print method (though currently only wrapper for print.NAVseries)

--- R/NAVseries.R
+++ R/NAVseries.R
@@ -1,24 +1,34 @@
 ## -*- truncate-lines: t; -*-
-## Time-stamp: <2014-02-11 09:55:10 CET (es)>
+## Time-stamp: <2014-02-21 17:02:01 CET (es)>
 
-NAVseries <- function(NAV, timestamp, description = NULL) {
+NAVseries <- function(NAV, timestamp,
+                      instrument = NULL,
+                      title = NULL,
+                      description = NULL) {
     if (missing(timestamp)) 
         timestamp <- seq_along(NAV)
+    ## if (is.null(title) && !is.null(instrument))
+    ##     title <- instrument
     ans <- list(NAV = NAV, timestamp = timestamp,
+                instrument = instrument,
+                title = as.character(title),
                 description = as.character(description))
-    if (any(duplicated(ans$NAV)))
+    if (any(duplicated(ans$timestamp)))
         warning("duplicated timestamps")
     class(ans) <- "NAVseries"
     ans
 }
 
 print.NAVseries <- function(x, ...) {
-    footnote <- FALSE
-    if (!is.null(x$description))
-        cat(x$description, "\n")
+    if (length(x$title))
+        cat(x$title, "\n") else if (!is.null(x$instrument))
+        cat(x$instrument, "\n")
+
+    ## TODO: assuming daily timestamps -- too restrictive
     timestamp <- aggregate(x$timestamp, by = list(as.Date(x$timestamp)), tail, 1L)[[2L]]
     NAV <- aggregate(x$NAV, by = list(as.Date(x$timestamp)), tail, 1L)[[2L]]
-    cat(as.character(min(timestamp)), " => ", as.character(max(timestamp)), "\n")
+    cat(as.character(min(timestamp)), " to ", as.character(max(timestamp)), "  |  ",
+        length(timestamp), " observations  |  ", sum(is.na(NAV)), " NAs\n", sep = "")
     minNAV <- min(NAV)
     if (length(ii <- which(NAV == minNAV)) > 1L) {
         minTS <- paste0(as.character(timestamp[ii][1L]), " (several dates)")
@@ -27,20 +37,33 @@ print.NAVseries <- function(x, ...) {
         minTS <- as.character(timestamp[ii])
     maxNAV <- max(NAV)
     if (length(ii <- which(NAV == maxNAV)) > 1L) {
-        maxTS <- paste0(as.character(timestamp[ii][1L]), " (several dates)")
+        maxTS <- paste0(as.character(timestamp[ii][1L]), ", several dates")
         footnote <- TRUE
     } else
         maxTS <- as.character(timestamp[ii])
     tmpN <- max(nchar(as.character(minNAV)), nchar(as.character(maxNAV)))
-    cat("lowest    ",  format(minNAV, width = tmpN), " @ ", minTS, "\n", sep = "")
-    cat("highest   ",  format(maxNAV, width = tmpN), " @ ", maxTS, "\n", sep = "")
+    cat("low  ",  format(minNAV, width = tmpN), " [", minTS, "]  |  ", sep = "")
+    cat("high ",  format(maxNAV, width = tmpN), " [", maxTS, "]\n", sep = "")
     cat("vol (ann.) ", format(round(sd(returns(NAV))*1600,1), nsmall = 1))
     cat("\n")
 
-
-    
-    cat("\nmonthly returns in %\n")
+        cat("\nmonthly returns in %\n")
     print(returns(NAV, timestamp, period = "monthly"), year.rows = FALSE)
+
+}
+
+summary.NAVseries <- function(object, ...) {
+
+    ans <- object
+    class(ans) <- "summary.NAVseries"
+    ans
+
+}
+
+print.summary.NAVseries <- function(x, ...) {
+    class(x) <- "NAVseries"
+    print(x)
+    invisible(x)
 }
 
 plot.NAVseries <- function(x, y, ...) {
@@ -52,5 +75,5 @@ plot.NAVseries <- function(x, y, ...) {
 }
 
 ## require("tseries"); require("zoo")
-## ## X <- get.hist.quote("AQRIX", quote = "AdjClose")
-## x <- NAVseries(coredata(X), index(X), description="AQR Risk Parity")
+## X <- get.hist.quote("AQRIX", quote = "AdjClose")
+## x <- NAVseries(coredata(X), index(X), instrument = "AQR Risk Parity")
