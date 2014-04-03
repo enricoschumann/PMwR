49f22c5b44ffcd3dec27b5c95f739ed46c915254
Enrico Schumann <es@enricoschumann.net>
2014-04-03 20:05:36 +0200

    (print.NAVseries): added method

--- R/NAVseries.R
+++ R/NAVseries.R
@@ -1,5 +1,5 @@
 ## -*- truncate-lines: t; -*-
-## Time-stamp: <2014-02-28 17:27:02 CET (es)>
+## Time-stamp: <2014-04-03 18:21:29 CEST (es)>
 
 NAVseries <- function(NAV, timestamp,
                       instrument = NULL,
@@ -19,35 +19,64 @@ NAVseries <- function(NAV, timestamp,
 
 print.NAVseries <- function(x, ...) {
     if (length(x$title))
-        cat(x$title, "\n") else if (!is.null(x$instrument))
+        cat(x$title, "\n")
+    else if (!is.null(x$instrument))
         cat(x$instrument, "\n")
+
+    template <-
+"%from% -- %to%         (%nobs% obs, %nna% NAs)
+Low      %low%  (%low_timestamp%)
+High    %high%  (%high_timestamp%)
+
+max DD    %dd%
+from    %dd_from%  (%dd_from_timestamp%)
+to      %dd_to%    (%dd_to_timestamp%)
+
+vol       %vol%
+up        %vol_up%
+down      %vol_down%\n"
 
     ## TODO: assuming daily timestamps -- too restrictive
     timestamp <- aggregate(x$timestamp, by = list(as.Date(x$timestamp)), tail, 1L)[[2L]]
     NAV <- aggregate(x$NAV, by = list(as.Date(x$timestamp)), tail, 1L)[[2L]]
-    cat(as.character(min(timestamp)), " to ", as.character(max(timestamp)), "  |  ",
-        length(timestamp), " observations  |  ", sum(is.na(NAV)), " NAs\n", sep = "")
-    minNAV <- min(NAV)
-    if (length(ii <- which(NAV == minNAV)) > 1L) {
-        minTS <- paste0(as.character(timestamp[ii][1L]), " (several dates)")
-        footnote <- TRUE
+
+    stats <- list()
+    stats$from <- as.character(min(timestamp))
+    stats$to   <- as.character(max(timestamp))
+    stats$nobs <- length(timestamp)
+    stats$nna  <- sum(is.na(NAV))
+    stats$low  <- min(NAV)
+    stats$high <- max(NAV)
+
+    tmp <- drawdown(x$NAV)
+    stats$dd <- format(round(100*tmp$maximum,1), nsmall = 1)
+    stats$
+    if (length(ii <- which(NAV == stats$low)) > 1L) {
+        stats$low_timestamp <- paste0(as.character(timestamp[ii][1L]),
+                                      " (several dates)")
+        footnote <- TRUE        
     } else
-        minTS <- as.character(timestamp[ii])
-    maxNAV <- max(NAV)
-    if (length(ii <- which(NAV == maxNAV)) > 1L) {
-        maxTS <- paste0(as.character(timestamp[ii][1L]), ", several dates")
-        footnote <- TRUE
+        stats$low_timestamp <- as.character(timestamp[ii])
+
+    if (length(ii <- which(NAV == stats$high)) > 1L) {
+        stats$high_timestamp <- paste0(as.character(timestamp[ii][1L]),
+                                       " (several dates)")
+        footnote <- TRUE        
     } else
-        maxTS <- as.character(timestamp[ii])
-    tmpN <- max(nchar(as.character(minNAV)), nchar(as.character(maxNAV)))
-    cat("low  ",  format(minNAV, width = tmpN), " [", minTS, "]  |  ", sep = "")
-    cat("high ",  format(maxNAV, width = tmpN), " [", maxTS, "]\n", sep = "")
-    cat("vol (ann.) ", format(round(sd(returns(NAV))*1600,1), nsmall = 1))
+        stats$high_timestamp <- as.character(timestamp[ii])
+
+    stats$vol      <- format(round(sd(returns(NAV))*1600,1), nsmall = 1)
+    stats$vol_up   <- format(round(pm(returns(NAV), normalise = TRUE, lower = FALSE)*1600,1), nsmall = 1)
+    stats$vol_down <- format(round(pm(returns(NAV), normalise = TRUE)*1600,1), nsmall = 1)
+    
+    for (s in names(stats)) {
+        template <- gsub(paste0("%", s, "%"), stats[[s]], template)
+    }
+    cat(template)
+    ## cat("\nMonthly returns in %\n")
     cat("\n")
-
-        cat("\nmonthly returns in %\n")
     print(returns(NAV, timestamp, period = "monthly"), year.rows = FALSE)
-
+    invisible(x)
 }
 
 summary.NAVseries <- function(object, ...) {
@@ -75,3 +104,12 @@ plot.NAVseries <- function(x, y, ...) {
 ## require("tseries"); require("zoo")
 ## X <- get.hist.quote("AQRIX", quote = "AdjClose")
 ## x <- NAVseries(coredata(X), index(X), instrument = "AQR Risk Parity")
+
+print.NAVseries <- function(x, y, ...) {
+    if (missing(y)) {
+        par(mar = c(3,3,1,1), las = 1)
+        plot(x$timestamp, x$NAV, type = "l")
+        
+
+    }
+}
