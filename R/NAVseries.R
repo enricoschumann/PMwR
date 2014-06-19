57f1e719ba6224b430b1b6d808413169b9f08409
Enrico Schumann <es@enricoschumann.net>
2014-06-19 18:19:27 +0200

    * R/NAVseries.R (print.summary.NAVseries): changed formating
    * man/NAVseries.Rd: added file

--- R/NAVseries.R
+++ R/NAVseries.R
@@ -1,5 +1,5 @@
 ## -*- truncate-lines: t; -*-
-## Time-stamp: <2014-06-18 20:06:58 CEST (es)>
+## Time-stamp: <2014-06-19 18:13:38 CEST (es)>
 
 NAVseries <- function(NAV, timestamp,
                       instrument = NULL,
@@ -33,18 +33,14 @@ print.NAVseries <- function(x, ...) {
     }
     n <- length(x$timestamp)
     na <- sum(is.na(x$NAV))
-    first <- x$NAV[1]
+    first <- x$NAV[1L]
     last <- x$NAV[n]
 
     cat(mint, "==>", maxt)
-    cat("   (",
-        format(n, big.mark = "."),
-        " data points, ", 
-        na,
-        " NAs)\n",
-        sep = "")
+    cat("   (", format(n, big.mark = "."), " data points, ", 
+        na, " NAs)\n", sep = "")
     cat(format(first, justify = "right", width = nchar(mint)), "   ",
-        format(last, justify = "right", width = nchar(maxt)), "\n")
+        format(last,  justify = "right", width = nchar(maxt)), "\n")
     invisible(x)
     
 }
@@ -58,21 +54,27 @@ summary.NAVseries <- function(object, ...) {
 
     ans <- list()
     ans$NAVseries <- object
+
     ans$from <- min(timestamp)
     ans$to   <- max(timestamp)
-    ans$nobs <- format(length(timestamp), big.mark = ".")
+    ans$nobs <- length(timestamp)
     ans$nna  <- sum(is.na(NAV))
     ans$low  <- min(NAV)
     ans$high <- max(NAV)
+    ans$low.when  <- timestamp[which.min(NAV)[1L]]
+    ans$high.when <- timestamp[which.max(NAV)[1L]]
     
-    tmp <- drawdown(object$NAV)
-    ans$dd      <- tmp$maximum
-    ans$dd_from <- tmp$high
-    ans$dd_to   <- tmp$low
-    ans$dd_from_timestamp <- datef(timestamp[tmp$high.position])
-    ans$dd_to_timestamp <- datef(timestamp[tmp$low.position])
-    ans$uw <- percf(1 - tail(NAV,1)/max(NAV))
+    tmp          <- drawdown(object$NAV)
+    ans$mdd      <- tmp$maximum
+    ans$mdd.high <- tmp$high
+    ans$mdd.low   <- tmp$low
+    ans$mdd.high.when <- timestamp[tmp$high.position]
+    ans$mdd.low.when   <- timestamp[tmp$low.position]
+    ans$underwater <- 1 - NAV[ans$nobs]/max(NAV)
 
+    ans$vol      <- sd(returns(NAV))*16
+    ans$vol.up   <- pm(returns(NAV), normalise = TRUE, lower = FALSE)*16
+    ans$vol.down <- pm(returns(NAV), normalise = TRUE)*16
 
     class(ans) <- "summary.NAVseries"
     ans
@@ -80,52 +82,61 @@ summary.NAVseries <- function(object, ...) {
 }
 
 print.summary.NAVseries <- function(x, ...) {
-    class(x) <- "NAVseries"
-    print(x)
-    invisible(x)
-
-    datef <- function(x)
-        format(x, "%d %b %Y")
+    datef <- function(x) {
+        if (inherits(x[1L], "Date"))
+            x <- format(x, "%d %b %Y")
+        x
+    }
     percf <- function(x)
         format(round(100*x, 1), nsmall = 1, justify = "right", width = 7)
     numf <- function(x)
         format(x, justify = "right", width = 7)
 
-    if (length(x$title))
-        cat(x$title, "\n")
-    else if (!is.null(x$instrument))
-        cat(x$instrument, "\n")
+    if (length(x$NAVseries$title))
+        cat(x$NAVseries$title, "\n")
+    else if (!is.null(x$NAVseries$instrument))
+        cat(x$NAVseries$instrument, "\n")
 
-    ans <- list()
-    ans$NAVseries <- object
-    stats <- list()
-    stats$from <- datef(min(timestamp))
-    stats$to   <- datef(max(timestamp))
-    stats$nobs <- format(length(timestamp), big.mark = ".")
-    stats$nna  <- sum(is.na(NAV))
-    stats$low  <- min(NAV)
-    stats$high <- max(NAV)
-    
-    tmp <- drawdown(object$NAV)
-    stats$dd <- percf(tmp$maximum)
-    stats$dd_from <- tmp$high
-    stats$dd_to <- tmp$low
-    stats$dd_from_timestamp <- datef(timestamp[tmp$high.position])
-    stats$dd_to_timestamp <- datef(timestamp[tmp$low.position])
-    stats$uw <- percf(1 - tail(NAV,1)/max(NAV))
+    ## format: dates
+    fields <- c("from", "to", "low.when", "high.when",
+                "mdd.high.when", "mdd.low.when")
+    for (f in fields)
+        x[[f]] <- datef(x[[f]])
 
+    ## format: %
+    fields <- c("mdd", "underwater",
+                "vol", "vol.up", "vol.down")
+    for (f in fields)
+        x[[f]] <- percf(x[[f]])
 
-    
+    cat("---------------------------------------------------------\n")
+    print(x$NAVseries)    
     template <-
-        c("%from% --> %to%         (%nobs% obs, %nna% NAs)",
-          "",
-          "Low    |  %low% | /%low_timestamp%",
-          "High   | %high% | /%high_timestamp%",
-          "",
-          "drawdown    %dd%                 vol   %vol%",
-          "from        %dd_from%  /%dd_from_timestamp%     up   %vol_up%",
-          "to          %dd_to%  /%dd_to_timestamp%     down %vol_down%",
-          "underwater  %uw%\n")
+        c("---------------------------------------------------------",
+          "High         %high%  (%high.when%)",
+          "Low           %low%  (%low.when%)",
+          "---------------------------------------------------------",
+          "max. drawdown (in %)  %mdd%",
+          "peak          %mdd.high%  (%mdd.high.when%)",
+          "trough        %mdd.low%  (%mdd.low.when%)",
+          "underwater  %underwater%",
+          "---------------------------------------------------------",
+          "volatility (in %, ann.)  %vol%",
+          "upside volatility        %vol.up%",
+          "downside volatility      %vol.down%",
+          "---------------------------------------------------------\n")
+    nx <- names(x)
+    nx <- nx[nx != "NAVseries"]
+    for (n in nx) {
+        template <- gsub(paste0("%", n, "%"), x[[n]], template)
+    }
+    cat(template, sep = "\n")
+    cat("Monthly returns  ")
+    sparkplot(returns(x$NAVseries$NAV))
+    cat("\n")
+    print(returns(x$NAVseries$NAV, x$NAVseries$timestamp, period = "month"),
+          year.rows = FALSE)
+    invisible(x)
 
 }
 
@@ -154,13 +165,7 @@ print.NAVseries <- function(x, ...) {
     
     cat(template, fill = TRUE)
     
-    ## cat("\nMonthly returns in %\n")
-
-    ## in_lines <- strsplit(template, "\n", TRUE)[[1L]]
-    ## in_lines[1] <- expstr(in_lines[1], after = "  +", width = 55)
-
     cat("\n")
-    ##print(returns(NAV, timestamp, period = "monthly"), year.rows = FALSE)
     invisible(x)
 }
 
@@ -168,20 +173,8 @@ print.NAVseries <- function(x, ...) {
 plot.NAVseries <- function(x, y, ...) {
     if (!missing(y))
         stop("scatterplot of *returns* -- not implemented")    
-    plot(x$timestamp, x$NAV, ...)
+    plot(x=x$timestamp, y=x$NAV, ...)
 
     return(invisible(x))
 }
 
-## require("tseries"); require("zoo")
-## X <- get.hist.quote("AQRIX", quote = "AdjClose")
-## x <- NAVseries(coredata(X), index(X), instrument = "AQR Risk Parity")
-
-## print.NAVseries <- function(x, y, ...) {
-##     if (missing(y)) {
-##         par(mar = c(3,3,1,1), las = 1)
-##         plot(x$timestamp, x$NAV, type = "l")
-        
-
-##     }
-## }
