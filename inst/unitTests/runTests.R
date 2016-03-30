85de9bcdba3edb4ae0cf0b2e02c6ec26f9ced342
Enrico Schumann <es@enricoschumann.net>
2016-03-30 15:28:33 +0200

    whether tests are run now depends on an environment variable

--- inst/unitTests/runTests.R
+++ inst/unitTests/runTests.R
@@ -1,15 +1,22 @@
-localTesting <- TRUE
-if (require("RUnit", quietly = TRUE)) {
-    require("PMwR")
-    if (localTesting)
-        path <- "~/Packages/PMwR/inst/unitTests" else
-    path <- system.file("unitTests", package = "PMwR")
-    myTestSuite <- defineTestSuite("PMwR",
+pkg <- "PMwR"
+if (tolower(Sys.getenv("ES_PACKAGE_TESTING")) == "true" &&
+    require("RUnit", quietly = TRUE) &&
+    require(pkg, quietly = TRUE, character.only = TRUE)) {
+
+    path <- "~/Packages/PMwR/inst/unitTests"
+
+    ## if the package is installed and you want to run
+    ## the test, use this path:    
+    ##
+    ##     path <- system.file("unitTests", package = "PMwR")
+    ##
+    
+    myTestSuite <- defineTestSuite(pkg,
                                    dirs = path,
                                    testFileRegexp = "ut_.*")
     stopifnot(isValidTestSuite(myTestSuite))
     testResult <- runTestSuite(myTestSuite, verbose = 0L)
-    printTextProtocol(testResult, showDetails = TRUE,
-                      fileName = paste(file.path(path, "report"),
-                      ".txt", sep = ""))
+    printTextProtocol(testResult,
+                      showDetails = TRUE,
+                      fileName = paste0(file.path(path, "test_results"), ".txt"))
 }
