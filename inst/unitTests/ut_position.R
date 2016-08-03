4ae34c4e80406b11bedef3e1d8169a6d2c67d80f
Enrico Schumann <es@enricoschumann.net>
2016-08-03 21:57:39 +0200

    Add tests for 'position'

--- inst/unitTests/ut_position.R
+++ inst/unitTests/ut_position.R
@@ -1,15 +1,27 @@
+test.position <- function() {
 
-require("PMwR")
+    ## require("PMwR")
+    ## require("RUnit")
 
-## construct from raw data
-t <- 1:5
-n <- c(1, 1, -1, 3, -4)
-position(amount = n, timestamp = t, when = 4.9)
-position(amount = n, timestamp = t, when = c(-10,1.4,4.9))
+    
+    checkEqualsNumeric(position(amount = 1:5), 15)
 
-## from journal
-j <- journal(timestamp = t, amount = n)
-position(j, when = 4.9)
-position(j, when = c(-10,1.4,4.9))
+    ## at least 'amount' needs to be specified
+    checkException(position(), silent = TRUE)
+
+    
+    ## construct from raw data or from journal
+    t <- 1:5
+    n <- c(1, 1, -1, 3, -4)
+    j <- journal(timestamp = t, amount = n)
+
+    position(amount = n, timestamp = t, when = 4.9)    
+
+    
+    position(amount = n, timestamp = t, when = c(-10,1.4,4.9))
+
+    position(j, when = 4.9)
+    position(j, when = c(-10,1.4,4.9))
 
 
+}
