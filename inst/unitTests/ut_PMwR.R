52797e71f5c1848eb7cdce0b0a40249fd45e54af
Enrico Schumann <es@enricoschumann.net>
2018-11-13 20:19:01 +0100

    Fix unit tests

--- inst/unitTests/ut_PMwR.R
+++ inst/unitTests/ut_PMwR.R
@@ -547,8 +547,6 @@ test.btest.tc <- function() {
 
 test.btest.journal <- function() {
 
-    require(PMwR)
-    require(RUnit)
     prices <- 1:10    
     signal <- function()
         if (Time() < 5)
@@ -556,22 +554,22 @@ test.btest.journal <- function() {
 
     j <- journal(btest(prices, signal))
     checkEquals(j,
-                dput(journal(amount = c(1, -1),
+                journal(amount = c(1, -1),
                         timestamp = c(2, 6),
                         instrument = "asset 1",
-                        price = c(2, 6))))
+                        price = c(2, 6)))
 
     j <- journal(btest(prices, signal, instrument = "A"))
     checkEquals(j,
-                dput(journal(amount = c(1, -1),
+                journal(amount = c(1, -1),
                         timestamp = c(2, 6),
                         instrument = "A",
-                        price = c(2, 6))))
+                        price = c(2, 6)))
     checkEquals(j,
-                dput(journal(amount = c(1, -1),
+                journal(amount = c(1, -1),
                         timestamp = c(2, 6),
                         instrument = c("A", "A"),
-                        price = c(2, 6))))
+                        price = c(2, 6)))
 
     prices <- 1:10
     prices <- cbind(A = prices, B = prices+0.5)
@@ -580,29 +578,29 @@ test.btest.journal <- function() {
             c(1,1) else c(0,0)
     j <- journal(btest(list(prices), signal))
     checkEquals(j,
-                dput(journal(amount = c(1, 1, -1, -1),
+                journal(amount = c(1, 1, -1, -1),
                         timestamp = c(2, 2, 6, 6),
                         instrument = c("A", "B", "A", "B"),
-                        price = c(2, 2.5, 6, 6.5))))
+                        price = c(2, 2.5, 6, 6.5)))
     
     j <- journal(btest(list(prices), signal,
                        ## overwrite instruments
                        instrument = c("a", "b"))) 
     checkEquals(j,
-                dput(journal(amount = c(1, 1, -1, -1),
+                journal(amount = c(1, 1, -1, -1),
                         timestamp = c(2, 2, 6, 6),
                         instrument = c("a", "b", "a", "b"),
-                        price = c(2, 2.5, 6, 6.5))))
+                        price = c(2, 2.5, 6, 6.5)))
     
     signal <- function()
         if (Time() < 5L)
             c(0,1) else c(0,0)
     j <- journal(btest(list(prices), signal))
     checkEquals(j,
-                dput(journal(amount = c(1, -1),
+                journal(amount = c(1, -1),
                         timestamp = c(2, 6),
                         instrument = "B",
-                        price = c(2.5, 6.5))))
+                        price = c(2.5, 6.5)))
 
 }
 
