c6f8f1f334748b2261a3fab4d5950feef3d841b3
Enrico Schumann <es@enricoschumann.net>
2020-01-16 08:03:07 +0100

    [btest] Replicate even if even if 'replications == 1'

---
+++ inst/tinytest/test_btest_replications.R
@@ -0,0 +1,20 @@
+## replications
+
+prices <- 1:10
+ans1 <- btest(prices, function() 1)
+expect_true(inherits(ans1, "btest"))
+
+ans <- btest(prices, function() 1, replications = 1)
+expect_equal(length(ans), 1)
+tmp <- ans[[1]]
+attr(tmp, "replication") <- NULL
+expect_equal(tmp, ans1)
+
+ans <- btest(prices, function() 1, replications = 2)
+expect_equal(length(ans), 2)
+tmp <- ans[[1]]
+attr(tmp, "replication") <- NULL
+expect_equal(tmp, ans1)
+tmp <- ans[[2]]
+attr(tmp, "replication") <- NULL
+expect_equal(tmp, ans1)
