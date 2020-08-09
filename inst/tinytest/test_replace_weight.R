eba059d7c75892f2e8b167d7dbba15ffc42d4a1f
Enrico Schumann <es@enricoschumann.net>
2020-08-09 09:44:38 +0200

    Add tests for 'rebalance'

--- inst/tinytest/test_replace_weight.R
+++ inst/tinytest/test_replace_weight.R
@@ -3,3 +3,16 @@ a <- c(x = 0.9, y = 0.05, z = 0.05)
 b <- c(A = 0.1, B = 0.2,  C = 0.7)
 expect_equal(1,
              sum(replace_weight(w, a = a, b = b, x = c(aaa = 1))))
+
+## -------------------
+
+n <- names(
+    replace_weight(w, a = a, b = b, x = c(aaa = 1),
+                   prefix = FALSE))
+expect_equal(n, c("aaa", "y", "z", "A", "B", "C"))
+
+
+n <- names(
+    replace_weight(w, a = a, b = b, x = c(aaa = 0.5, bbb = 0.5),
+                   prefix = FALSE))
+expect_equal(n, c("aaa", "bbb", "y", "z", "A", "B", "C"))
