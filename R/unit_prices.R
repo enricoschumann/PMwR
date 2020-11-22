dc8fce7e3d3c5b66e7ae10e1e50b60d2e834e7ed
Enrico Schumann <es@enricoschumann.net>
2020-11-22 10:24:13 +0100

    [unit_prices] Rewrite function
    See ChangeLog for details.

--- R/unit_prices.R
+++ R/unit_prices.R
@@ -1,11 +1,91 @@
 ## -*- truncate-lines: t; -*-
-## Copyright (C) 2008-18  Enrico Schumann
+## Copyright (C) 2008-20  Enrico Schumann
 
 unit_prices <- function(NAV, cashflows,
+                        initial.price,
+                        initial.shares = 0,
+                        cf.included = TRUE) {
+
+    ## result 'price' series
+    ## + attr (data.frame) transactions
+    ##                     - timestamp
+    ##                     - id
+    ##                     - shares
+    ##                     - price
+
+    if (!cf.included)
+        .NotYetUsed("cf.included")
+    if (initial.shares != 0)
+        .NotYetUsed("initial.shares")
+
+    if (inherits(NAV, "zoo"))
+        NAV <- data.frame(index(NAV), NAV)
+
+    if (inherits(cashflows, "zoo"))
+        cashflows <- data.frame(index(cashflows), cashflows)
+
+    if (!cf.included) {
+
+    }
+    
+    shares <- numeric(nrow(cashflows))
+    all.t <- sort(unique(cashflows[[1]]))
+    t.NAV <- match(all.t, NAV[[1]])
+
+    if (any(is.na(t.NAV)))
+        stop("cashflow without matching NAV timestamp")
+
+    price <- numeric(length(all.t))
+    if (initial.shares == 0) {
+        if (missing(initial.price))
+            initial.price <- 100
+        price[1] <- initial.price
+    } else
+        price[1] <- (NAV[t.NAV[1]] - cf.included*sum(cashflows[[2]][1]))/
+            initial.shares
+    shares[all.t[1] == cashflows[[1]]] <- cashflows[[2]][cashflows[[1]] == all.t[1]]/price[1] +
+        initial.shares
+    if (length(all.t) > 1L) {
+        sum.shares <- sum(shares[all.t[1] == cashflows[[1]]])
+        for (t in all.t[-1]) {
+            i <- t == all.t
+            price[i] <- (NAV[[2]][t.NAV[i]] -
+                         cf.included*sum(cashflows[[2]][cashflows[[1]] == t]))/
+                sum.shares
+            shares[t == cashflows[[1]]] <- cashflows[[2]][cashflows[[1]] == t]/price[i]
+            sum.shares <- sum.shares + sum(shares[t == cashflows[[1]]])
+        }
+    }
+
+    total.shares <- numeric(nrow(NAV))
+    total.shares[t.NAV] <- tapply(shares, cashflows[[1]], sum)
+    total.shares <- cumsum(total.shares)
+
+    res <- NAV
+    names(res) <- c("timestamp", "NAV")
+    res <- cbind(res,
+                 data.frame(price = NAV[[2]]/total.shares,
+                            units = total.shares,
+                            stringsAsFactors = FALSE))
+
+    colnames(cashflows) <- if (ncol(cashflows) == 3L)
+                               c("timestamp", "cashflow", "account")
+                           else
+                               c("timestamp", "cashflow")
+    attr(res, "transactions") <- cbind(cashflows,
+                                       units = shares)
+    res
+}
+
+.unit_prices <- function(NAV, cashflows,
                         initial.price = 100,
                         initial.shares = 0,
                         cf.included = FALSE) {
 
+    .Deprecated("unit_prices",
+                msg = paste0(sQuote(".unit_prices"), " is deprecated.\n",
+                             "Use ", sQuote("unit_prices"), " instead."))
+    
     if (initial.shares != 0)
         .NotYetUsed("initial.shares")
     res <- NAV
--- inst/tinytest/test_unit_prices.R
+++ inst/tinytest/test_unit_prices.R
@@ -17,3 +17,32 @@ NAV <- data.frame(timestamp = seq(as.Date("2017-1-1"),
 x2 <- unit_prices(NAV, cf, cf.included = FALSE)
 
 expect_equal(x1$price,x2$price)
+
+
+####
+
+NAV <- data.frame(1:3,
+                  c(100,100,200))
+cf <- data.frame(3, 100)
+unit_prices(NAV, cf, cf.included = TRUE, initial.price = 200)
+
+
+NAV <- data.frame(1:3,
+                  c(101,101,101))
+cf <- data.frame(1, 100)
+unit_prices(NAV, cf, cf.included = TRUE, initial.price = 100)
+
+
+####
+
+NAV <- data.frame(1:3, c(100,100,200))
+cf <- data.frame(t = c(1,1,3,3),
+                 cf = c(80,20,50,50),
+                 id = c(1,2,1,2))
+unit_prices(NAV, cf, cf.included = TRUE, initial.price = 100)
+
+NAV <- data.frame(1:3, c(100,100,100))
+cf <- data.frame(t = c(1,1,3,3),
+                 cf = c(80,20,-80,80),
+                 id = c(1,2,1,2))
+unit_prices(NAV, cf, cf.included = TRUE, initial.price = 100)
