126dccad195272f7294d6e907dd350eef42fa4d1
Enrico Schumann <es@enricoschumann.net>
2024-01-24 12:37:46 +0100

    Add tests

--- inst/tinytest/test_is_valid_ISIN.R
+++ inst/tinytest/test_is_valid_ISIN.R
@@ -9,3 +9,29 @@ expect_equal(unname(is_valid_ISIN(isin)),
 expect_equal(unname(is_valid_ISIN(c("US0378331005",
                                     "us0378331005"))),
              c(TRUE, TRUE))
+
+isin <- c("US0378331005",
+          "AU00x0XVGZxx")
+expect_equal(unname(is_valid_ISIN(isin)),
+             c(TRUE, FALSE))
+
+isin <- character(0L)
+expect_equal(unname(is_valid_ISIN(isin)),
+             logical(0L))
+
+isin <- NA
+expect_equal(unname(is_valid_ISIN(isin)),
+             FALSE)
+
+isin <- NA
+expect_equal(unname(is_valid_ISIN(isin, NA.FALSE = FALSE)),
+             FALSE)
+
+
+isin <- c(NA, "US0378331005")
+expect_equal(unname(is_valid_ISIN(isin)),
+             c(NA, TRUE))
+
+isin <- c(NA, "US0378331005")
+expect_equal(unname(is_valid_ISIN(isin, NA.FALSE = TRUE)),
+             c(FALSE, TRUE))
