540eaa8db2dd31180cb48762cca22e74cca8b12b
Enrico Schumann <es@enricoschumann.net>
2024-11-08 15:24:09 +0100

    Add support for rare cases in btest
    Several cases with initial position, NAs in prices
    without position and burnin of 0 are now supported.

--- inst/tinytest/test_btest_initial.position.R
+++ inst/tinytest/test_btest_initial.position.R
@@ -7,7 +7,6 @@ expect_equal(
                          b = 0))),
     0) ## => there should be no trade at all
 
-
 ## -------------------------------------------
 
 
@@ -45,3 +44,48 @@ bt <- btest(list(P),
             b = 0,
             do.signal = TRUE)
 expect_equivalent(bt$wealth, c(P %*% c(1, 2)))
+
+
+## -------------------------------------------
+
+P <- 100:110
+bt0 <- btest(P,
+             initial.position = 1,
+             b = 0,
+             do.signal = FALSE)
+bt1 <- btest(P,
+             initial.position = 1,
+             b = 1,
+             do.signal = FALSE)
+expect_equal(bt0$wealth, bt1$wealth)
+expect_equal(bt0$wealth, 100:110)
+
+
+## -------------------------------------------
+
+P <- 100:110
+P <- cbind(P, NA)
+bt <- btest(list(P),
+            initial.position = c(1, 0),
+            b = 0,
+            do.signal = FALSE)
+expect_equal(bt$wealth, 100:110)
+
+bt <- btest(list(P),
+            initial.position = c(1, 0),
+            b = 0,
+            signal = function() NULL)
+expect_equal(bt$wealth, 100:110)
+
+debug(btest)
+bt <- btest(list(P),
+            initial.position = c(1, 0),
+            b = 0,
+            do.signal = function() TRUE,
+            signal = function() c(0.5,0))
+
+expect_equal(bt$wealth,
+             c(100.0, 100.5, 101.0, 101.5, 102, 102.5,
+               103.0, 103.5, 104.0, 104.5, 105))
+expect_equal(bt$cash,
+             rep(50, nrow(P)))
