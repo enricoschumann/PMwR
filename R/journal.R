2d386a3352863a4a8ed50457ce458dec52823037
Enrico Schumann <es@enricoschumann.net>
2025-09-03 16:08:17 +0200

    Move 'instrument' code to separate file

--- R/journal.R
+++ R/journal.R
@@ -240,48 +240,6 @@ amount <- function(x, ...) {
     x
 }
 
-instrument <- function(x, ...) {
-    UseMethod("instrument")
-}
-
-instrument.position <- function(x, ...) {
-    attr(x, "instrument")
-}
-
-instrument.pricetable <- function(x, ...) {
-    attr(x, "instrument")
-}
-
-instrument.journal <- function(x, ...) {
-    x$instrument
-}
-
-instrument.NAVseries <- function(x, ...) {
-    attr(x, "instrument")
-}
-
-
-`instrument<-` <- function(x, ..., value) {
-    UseMethod("instrument<-")
-}
-
-`instrument<-.journal` <- function(x, ..., value) {
-    len <- length(value)
-    lenx <- length(x)
-    if (len == lenx)
-        x$instrument <- value
-    else if (len == 1L)
-        x$instrument <- rep(value, lenx)
-    else
-        stop("length(instrument) differs from length(journal)")
-    x
-}
-
-`instrument<-.NAVseries` <- function(x, ..., value) {
-    attr(x, "instrument") <- value
-    x
-}
-
 summary.journal <- function(object,
                             by = "instrument",
                             drop.zero = TRUE,
