f8d4e81833bd668bc228e8659baed56dabdb10e2
Enrico Schumann <es@enricoschumann.net>
2026-03-17 11:30:16 +0100

    Add tests
    Check whether total wealth can be reconstructed from
    positions + cash.

--- inst/tinytest/test_btest_position.R
+++ inst/tinytest/test_btest_position.R
@@ -10,6 +10,8 @@ for (i in 1:20) {
                  unname(as.matrix(position(bt))))
 }
 
+
+
 ## two instruments
 prices <- cbind(a = 1:10,
                 b = 101:110)
@@ -24,3 +26,44 @@ for (i in 1:20) {
                                            when = 1:10))),
                  unname(as.matrix(position(bt))))
 }
+
+
+
+## replicate wealth from position (amounts)
+na <- 5
+nt <- 10
+prices <- rnorm(na*nt, sd = 0.01)
+dim(prices) <- c(nt, na)
+prices <- apply(prices, 2, function(x) cumprod(x + 1))
+
+signal <- function() {
+    n <- ncol(Close())
+    sample(0:5, n, replace = TRUE)
+}
+
+bt <- btest(list(prices), signal = signal, b = 0)
+p <- position(bt, include.cash = TRUE)
+expect_equivalent(rowSums(p*cbind(prices, 1)), bt$wealth)
+
+
+
+## replicate scaled wealth from weights
+na <- 5
+nt <- 10
+prices <- rnorm(na*nt, sd = 0.01)
+dim(prices) <- c(nt, na)
+prices <- apply(prices, 2, function(x) cumprod(x + 1))
+
+signal <- function() {
+    n <- ncol(Close())
+    sample(0:5, n, replace = TRUE)
+}
+
+bt <- btest(list(prices), signal = signal, b = 0, initial.cash = 100)
+p <- position(bt, include.cash = TRUE)
+weights <- (p*cbind(prices, 1)) / bt$wealth
+
+r <- returns(cbind(prices, 1), pad = 0)
+
+expect_equivalent(rowSums(weights[-nrow(weights), ] * r[-1, ]),
+                  returns(bt$wealth))
